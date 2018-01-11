824c06df2bb6c9ae50b52a58abca9e822d300869
Milan Curcic <caomaco@gmail.com>
2018-01-11 07:23:48 -0500

    update to tag 2b

--- src/app/tsunami.f90
+++ src/app/tsunami.f90
@@ -1,20 +1,18 @@
 program tsunami
 
+use iso_fortran_env, only: int32, real32
+
 implicit none
 
-integer :: i, n ! indices in space and time
-integer :: im ! grid size in space
-integer :: nm ! number of time steps
+integer(kind=int32) :: i, n
 
-real :: dt ! time step [s]
-real :: dx ! grid spacing [m]
-real :: c ! phase speed [m/s]
+integer(kind=int32), parameter :: im = 100 ! grid size in space
+integer(kind=int32), parameter :: nm = 100 ! number of time steps
 
-im = 100
-nm = 100
+real(kind=real32), parameter :: dt = 1 ! time step [s]
+real(kind=real32), parameter :: dx = 1 ! grid spacing [m]
+real(kind=real32), parameter :: c = 1 ! phase speed [m/s]
 
-dt = 1.
-dx = 1.
-c = 1.
+real(kind=real32), dimension(im) :: du, u
 
 end program tsunami
