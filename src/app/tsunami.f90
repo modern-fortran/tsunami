91ff1b33743a50408c64bf23e43601c0467895ba
Milan Curcic <caomaco@gmail.com>
2018-01-11 07:17:58 -0500

    update for tag 2a

--- src/app/tsunami.f90
+++ src/app/tsunami.f90
@@ -1,2 +1,20 @@
 program tsunami
+
+implicit none
+
+integer :: i, n ! indices in space and time
+integer :: im ! grid size in space
+integer :: nm ! number of time steps
+
+real :: dt ! time step [s]
+real :: dx ! grid spacing [m]
+real :: c ! phase speed [m/s]
+
+im = 100
+nm = 100
+
+dt = 1.
+dx = 1.
+c = 1.
+
 end program tsunami
