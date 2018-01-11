606433bf6b12332cf94340b4b717e4af78a6d007
Milan Curcic <caomaco@gmail.com>
2018-01-11 07:41:40 -0500

    update to tag 2c

--- src/app/tsunami.f90
+++ src/app/tsunami.f90
@@ -1,12 +1,22 @@
 program tsunami
 
-use iso_fortran_env, only: int32, real32
+! Tsunami simulator
+!
+! This version solves the linearized 1-d advection equation:
+!
+!     du/dt + c du/dx = 0
+!
+! Discretized as:
+!
+!     u^{n+1}_i = u^n_i - c * dt * (u^n_{i} - u^n_{i-1}) / dx
+
+use iso_fortran_env, only: int32, real32, output_unit
 
 implicit none
 
 integer(kind=int32) :: i, n
 
-integer(kind=int32), parameter :: im = 100 ! grid size in space
+integer(kind=int32), parameter :: im = 100 ! grid size in x
 integer(kind=int32), parameter :: nm = 100 ! number of time steps
 
 real(kind=real32), parameter :: dt = 1 ! time step [s]
@@ -14,5 +24,33 @@ real(kind=real32), parameter :: dx = 1 ! grid spacing [m]
 real(kind=real32), parameter :: c = 1 ! phase speed [m/s]
 
 real(kind=real32), dimension(im) :: du, u
+
+! initialize a gaussian blob centered at i = 25
+do i = 1, im
+  u(i) = exp(-0.02 * (i - 25)**2)
+end do
+
+! write initial state to screen
+write(unit=output_unit, fmt=*) 0, u
+
+time_loop: do n = 1, nm
+
+  ! apply the periodic boundary condition
+  du(1) = u(1) - u(im)
+
+  ! calculate the difference of u in space
+  do concurrent (i = 2:im)
+    du(i) = u(i) - u(i-1)
+  end do
+
+  ! compute u at next time step
+  do concurrent (i = 1:im)
+    u(i) = u(i) - c * du(i) / dx * dt
+  end do
+
+  ! write current state to screen
+  write(unit=output_unit, fmt=*) n, u
+
+end do time_loop
 
 end program tsunami
