5edca2f0f6ca8bd13f45229970121eb79a85aab2
Milan Curcic <caomaco@gmail.com>
2018-05-30 11:21:03 -0400

    use static arrays in diff functions

--- src/lib/mod_diff.f90
+++ src/lib/mod_diff.f90
@@ -14,12 +14,11 @@ contains
   pure function diffc(x) result(dx)
     ! Returns a centered difference of a 1-d array,
     ! without any boundary conditions applied.
-    real(kind=rk), dimension(:), intent(in) :: x
-    real(kind=rk), dimension(:), allocatable :: dx
-    integer(kind=ik) :: i, idm
+    real(rk), intent(in) :: x(:)
+    real(rk) :: dx(size(x))
+    integer(ik) :: i, idm
 
     idm = size(x)
-    allocate(dx(idm))
     dx = 0
 
     do concurrent(i = 2:idm-1)
@@ -31,12 +30,11 @@ contains
   pure function diffc_periodic(x) result(dx)
     ! Returns a centered difference of a 1-d array,
     ! with periodic boundary condition.
-    real(kind=rk), dimension(:), intent(in) :: x
-    real(kind=rk), dimension(:), allocatable :: dx
-    integer(kind=ik) :: i, idm
+    real(rk), intent(in) :: x(:)
+    real(rk) :: dx(size(x))
+    integer(ik) :: i, idm
 
     idm = size(x)
-    allocate(dx(idm))
 
     dx(1) = 0.5 * (x(2) - x(idm))
     dx(idm) = 0.5 * (x(1) - x(idm-1))
@@ -50,12 +48,11 @@ contains
   pure function diffu(x) result(dx)
     ! Returns an upstream difference of a 1-d array,
     ! with periodic boundary condition.
-    real(kind=rk), dimension(:), intent(in) :: x
-    real(kind=rk), dimension(:), allocatable :: dx
-    integer(kind=ik) :: i, idm
+    real(rk), intent(in) :: x(:)
+    real(rk) :: dx(size(x))
+    integer(ik) :: i, idm
 
     idm = size(x)
-    allocate(dx(idm))
 
     dx(1) = x(1) - x(idm)
 
