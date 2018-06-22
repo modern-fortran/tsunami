e31095849cb6bfb37fe17c89d23f80d5a45fe4d3
Milan Curcic <caomaco@gmail.com>
2018-06-22 17:35:37 -0400

    simple 2-d contourf plot

---
+++ plotting/plot_2d_field.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('input_file', help='binary file output by tsunami simulator')
+args = parser.parse_args()
+
+input_file = args.input_file
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+dims = 102, 102
+field = np.reshape(np.fromfile(input_file, dtype='float32'), dims)
+
+fig = plt.figure(figsize=(8,7))
+ax = fig.add_subplot(111, aspect='equal')
+plt.contourf(field[1:-2, 1:-2], np.arange(-1, 1.05, 0.05))
+plt.title(input_file)
+plt.savefig(input_file[:-2] + '.png')
+plt.close(fig)
