c31eebaeaaf15b00ac87a41efeac7ae492ed9655
Milan Curcic <caomaco@gmail.com>
2018-10-13 13:29:14 -0400

    almost working; not bit-for-bit reproducible between different number of images

--- src/app/tsunami.f90
+++ src/app/tsunami.f90
@@ -12,19 +12,18 @@ program tsunami
 
   use iso_fortran_env, only: output_unit
 
-  use mod_boundary, only: reflective_boundary
   use mod_diagnostics, only: ke, mean
   use mod_diff, only: diffx => diffc_2d_x, diffy => diffc_2d_y
   use mod_io, only: write_field
   use mod_kinds, only: ik, rk
-  use mod_parallel, only: num_tiles, tile_indices, tile_neighbors_2d, update_halo, allocate_coarray
+  use mod_parallel, only: tile_indices, update_halo
 
   implicit none
 
   integer(ik) :: i, j, n
 
-  integer(ik), parameter :: im = 100 ! grid size in x
-  integer(ik), parameter :: jm = 100 ! grid size in y
+  integer(ik), parameter :: im = 101 ! grid size in x
+  integer(ik), parameter :: jm = 101 ! grid size in y
   integer(ik), parameter :: nm = 1000 ! number of time steps
 
   real(rk), parameter :: dt = 0.02 ! time step [s]
@@ -33,33 +32,17 @@ program tsunami
 
   real(rk), parameter :: g = 9.8 ! gravitational acceleration [m/s]
 
-  !real(rk), allocatable :: h(:,:)[:], u(:,:)[:], v(:,:)[:]
   real(rk), allocatable :: h(:,:), u(:,:), v(:,:)
-  !real(rk), allocatable :: gather(:,:)[:]
-  real(rk), allocatable :: gather(:,:)
+  real(rk), allocatable :: gather(:,:)[:]
   real(rk), allocatable :: hmean(:,:)
-
-  !real(rk), allocatable :: halo(:)[:]
 
   integer(ik), parameter :: ipos = 51, jpos = 51
   real(rk), parameter :: decay = 0.02
 
-  integer(ik) :: is, ie ! global start and end indices
-  integer(ik) :: ils, ile ! local start and end computational indices
-  integer(ik) :: ims, ime ! local start and end memory indices
-
-  integer(ik) :: js, je ! global start and end indices
-  integer(ik) :: jls, jle ! local start and end computational indices
-  integer(ik) :: jms, jme ! local start and end memory indices
-
-  integer(ik) :: tile_size
-
-  integer(ik) :: indices(4), neighbors(4)
+  integer(ik) :: is, ie, js, je ! global start and end indices
+  integer(ik) :: indices(4)
 
   if (this_image() == 1) print *, 'Tsunami started'
-
-  print *, 'Tile neighbors:', this_image(), tile_neighbors_2d(periodic=.true.)
-  sync all
 
   indices = tile_indices([im, jm])
   is = indices(1)
@@ -72,82 +55,46 @@ program tsunami
   allocate(v(is-1:ie+1, js-1:je+1))
   allocate(hmean(is-1:ie+1, js-1:je+1))
 
-  allocate(gather(im, jm))
+  allocate(gather(im, jm)[*])
 
   ! initialize a gaussian blob centered at i = 25
   do concurrent(i = is-1:ie+1, j = js-1:je+1)
-    h(i-is+1, j-js+1) = exp(-decay * ((i - ipos)**2 + (j - jpos)**2))
+    h(i, j) = exp(-decay * ((i - ipos)**2 + (j - jpos)**2))
   end do
-
-  !print *, this_image(), 'before allocate_coarray'
-  !call allocate_coarray()
-  !print *, this_image(), 'after allocate_coarray'
-  !stop
 
   ! set initial velocity and mean water depth
   u = 0
   v = 0
   hmean = 10
 
-  ! gather to image 1 and write current state to screen
-  !gather(is:ie, js:je)[1] = h(ils:ile, jls:jle)
-  !sync all
-  !if (this_image() == 1) write(unit=output_unit, fmt=*) 0, gather
-
   time_loop: do n = 1, nm
-
-    ! update halo for h
-    !h(ime,:)[left] = h(ils,:)
-    !h(ims,:)[right] = h(ile,:)
-    !h(:,jme)[left] = h(:,jls)
-    !h(:,jms)[right] = h(:,jle)
-    !sync all
 
     call update_halo(h, indices)
 
     ! compute u at next time step
-    u = u - (u * diffx(u) / dx&
-           + v * diffy(u) / dy&
-           + g * diffx(h) / dx) * dt
+    u = u - (u * diffx(u) / dx + v * diffy(u) / dy &
+      + g * diffx(h) / dx) * dt
 
-    v = v - (u * diffx(v) / dx&
-           + v * diffy(v) / dy&
-           + g * diffy(h) / dy) * dt
+    ! compute v at next time step
+    v = v - (u * diffx(v) / dx + v * diffy(v) / dy &
+      + g * diffy(h) / dy) * dt
 
-    !sync all
-
-    ! update halo for u
-    !u(ime,:)[left] = u(ils,:)
-    !u(ims,:)[right] = u(ile,:)
-    !u(:,jme)[left] = u(:,jls)
-    !u(:,jms)[right] = u(:,jle)
-
-    ! update halo for v
-    !v(ime,:)[left] = v(ils,:)
-    !v(ims,:)[right] = v(ile,:)
-    !v(:,jme)[left] = v(:,jls)
-    !v(:,jms)[right] = v(:,jle)
-    !sync all
+    sync all
     call update_halo(u, indices)
     call update_halo(v, indices)
 
     ! compute h at next time step
     h = h - diffx(u * (hmean + h)) / dx * dt&
           - diffy(v * (hmean + h)) / dy * dt
+    sync all
 
-    !call reflective_boundary(u, v, h)
-
-    ! gather to image 1 and write current state to screen
-    !gather(is:ie)[1] = h(ils:ile)
-    !sync all
-    !if (this_image() == 1) write(unit=output_unit, fmt=*) n, gather
-
-    !print *, n, h(50, 20), u(50, 20), v(50, 20)
-    print *, n, mean(h), mean(ke(u, v))
-
-    !call write_field(h, 'h', n)
-    !call write_field(u, 'u', n)
-    !call write_field(v, 'v', n)
+    ! gather to image 1 and write water height to file
+    gather(is:ie, js:je)[1] = h(is:ie, js:je)
+    sync all
+    if (this_image() == 1) then
+      print *, n, gather(3 * im / 4, jm / 2)
+      call write_field(gather, 'h', n)
+    end if
 
   end do time_loop
 
