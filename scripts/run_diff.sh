e68a9e44e9e8940863c0ace422a65d00198c8baf
Milan Curcic <caomaco@gmail.com>
2018-10-16 12:03:41 -0400

    bug fix and echo in script

--- scripts/run_diff.sh
+++ scripts/run_diff.sh
@@ -6,13 +6,15 @@
 
 # run the program in sequence
 for n in {1..4}; do
+  echo Running app on $n images..
   mpiexec --oversubscribe -n $n ./app > out_${n}.txt
 done
 
 # compare the output
 for n in {2..4}; do
-  echo diff between num_images = 1 and 2
+  echo diff between num_images = 1 and 2:
   diff out_[12].txt
+done
 
 # remove temporary output files
 rm out_*.txt
