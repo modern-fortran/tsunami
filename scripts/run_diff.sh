ff1c5cbfa3be5f52bd4aba34761848400b032af0
Milan Curcic <caomaco@gmail.com>
2018-10-16 11:59:46 -0400

    add script to run and compare the output

---
+++ scripts/run_diff.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Will run the program on 1, 2, 3, and 4 parallel 
+# images in sequence and save the output. It will
+# then diff the output to catch for any differences.
+
+# run the program in sequence
+for n in {1..4}; do
+  mpiexec --oversubscribe -n $n ./app > out_${n}.txt
+done
+
+# compare the output
+for n in {2..4}; do
+  echo diff between num_images = 1 and 2
+  diff out_[12].txt
+
+# remove temporary output files
+rm out_*.txt
