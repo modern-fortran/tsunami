84d15a356a84d64052f385a518ef7c39095893f1
Milan Curcic <caomaco@gmail.com>
2018-11-02 11:34:17 -0400

    retire old diff functions

--- src/lib/mod_diff.f90
+++ src/lib/mod_diff.f90
@@ -1,18 +1,18 @@
 module mod_diff
 
-  ! Module that provides finite difference functions.
+  ! Finite difference functions.
 
   use mod_kinds, only: ik, rk
 
   implicit none
 
   private
-  public :: diffc, diffc_periodic, diffu
   public :: diffx, diffy
 
 contains
 
   pure function diffx(x) result(dx)
+    ! Centered finite difference in x.
     real(rk), intent(in) :: x(:,:)
     real(rk) :: dx(size(x, dim=1), size(x, dim=2))
     integer(ik) :: i, im
@@ -24,6 +24,7 @@ contains
   end function diffx
 
   pure function diffy(x) result(dx)
+    ! Centered finite difference in y.
     real(rk), intent(in) :: x(:,:)
     real(rk) :: dx(size(x, dim=1), size(x, dim=2))
     integer(ik) :: j, jm
@@ -33,52 +34,5 @@ contains
       dx(:,j) = 0.5 * (x(:,j+1) - x(:,j-1))
     end do
   end function diffy
-
-  pure function diffc(x) result(dx)
-    ! Returns a centered difference of a 1-d array,
-    ! without any boundary conditions applied.
-    real(rk), intent(in) :: x(:)
-    real(rk) :: dx(size(x))
-    integer(ik) :: i, idm
-
-    idm = size(x)
-    dx = 0
-
-    do concurrent(i = 2:idm-1)
-      dx(i) = 0.5 * (x(i+1) - x(i-1))
-    end do
-
-  end function diffc
-
-  pure function diffc_periodic(x) result(dx)
-    ! Returns a centered difference of a 1-d array,
-    ! with periodic boundary condition.
-    real(rk), intent(in) :: x(:)
-    real(rk) :: dx(size(x))
-    integer(ik) :: i, idm
-
-    idm = size(x)
-
-    dx(1) = 0.5 * (x(2) - x(idm))
-    dx(idm) = 0.5 * (x(1) - x(idm-1))
-
-    do concurrent(i = 2:idm-1)
-      dx(i) = 0.5 * (x(i+1) - x(i-1))
-    end do
-
-  end function diffc_periodic
-
-  pure function diffu(x) result(dx)
-    ! Returns an upstream difference of a 1-d array,
-    ! with periodic boundary condition.
-    real(rk), intent(in) :: x(:)
-    real(rk) :: dx(size(x))
-    integer(ik) :: i, idm
-    idm = size(x)
-    dx(1) = x(1) - x(idm)
-    do concurrent(i = 2:idm)
-      dx(2:idm) = x(2:idm) - x(1:idm-1)
-    end do
-  end function diffu
 
 end module mod_diff
