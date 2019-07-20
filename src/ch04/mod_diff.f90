d2221b0ece1ecc5821a1ce47725d6accd9602a55
Milan Curcic <caomaco@gmail.com>
2019-07-20 12:12:30 -0400

    update ch4 tsunami to shallow water gravity wave

--- src/ch04/mod_diff.f90
+++ src/ch04/mod_diff.f90
@@ -4,18 +4,33 @@ module mod_diff
   implicit none
 
   private
-  public :: diff
+  public :: diffu, diffc
 
 contains
 
-  pure function diff(x) result(dx)
-    ! Returns a 1st-order upstream finite difference of a 1-d array.
+  pure function diffc(x) result(dx)
+    ! Returns a 2nd order centered difference of a 1-d array,
+    ! with periodic boundary condition.
+    real(real32), intent(in) :: x(:)
+    real(real32) :: dx(size(x))
+    integer(int32) :: im
+    im = size(x)
+    dx(1) = x(2) - x(im)
+    dx(im) = x(1) - x(im-1)
+    dx(2:im-1) = x(3:im) - x(1:im-1)
+    dx = 0.5 * dx
+  end function diffc
+
+
+  pure function diffu(x) result(dx)
+    ! Returns a 1st-order upstream finite difference of a 1-d array,
+    ! with periodic boundary condition.
     real(real32), intent(in) :: x(:)
     real(real32) :: dx(size(x))
     integer(int32) :: im
     im = size(x)
     dx(1) = x(1) - x(im)
     dx(2:im) = x(2:im) - x(1:im-1)
-  end function diff
+  end function diffu
 
 end module mod_diff
--- src/ch04/tsunami.f90
+++ src/ch04/tsunami.f90
@@ -8,38 +8,47 @@ program tsunami
   ! are abstracted as external procedures.
 
   use iso_fortran_env, only: int32, real32
-  use mod_diff, only: diff
+  use mod_diff, only: diff => diffc
   use mod_initial, only: set_gaussian_blob
 
   implicit none
 
-  integer(int32) :: i, n
+  integer(int32) :: n
 
   integer(int32), parameter :: im = 100 ! grid size in x
-  integer(int32), parameter :: nm = 100 ! number of time steps
+  integer(int32), parameter :: nm = 5000 ! number of time steps
 
-  real(real32), parameter :: dt = 1 ! time step [s]
+  real(real32), parameter :: dt = 0.02 ! time step [s]
   real(real32), parameter :: dx = 1 ! grid spacing [m]
-  real(real32), parameter :: c = 1 ! phase speed [m/s]
+  real(real32), parameter :: g = 9.8 ! gravitational acceleration [m/s^2]
 
-  real(real32) :: u(im)
+  real(real32) :: h(im), hmean(im), u(im)
 
   integer(int32), parameter :: icenter = 25
   real(real32), parameter :: decay = 0.02
 
-  ! initialize to a Gaussian blob
-  call set_gaussian_blob(u, icenter, decay)
+  ! initialize water height to a Gaussian blob
+  call set_gaussian_blob(h, icenter, decay)
+
+  ! initialize water velocity to zero
+  u = 0
+
+  ! initialize mean water depth to 10 m
+  hmean = 10
 
   ! write initial state to screen
-  print *, 0, u
+  print *, 0, h
 
   time_loop: do n = 1, nm
 
     ! compute u at next time step
-    u = u - c * diff(u) / dx * dt
+      u = u - (u * diff(u) + g * diff(h)) / dx * dt
+
+    ! compute h at next time step
+    h = h - diff(u * (hmean + h)) / dx * dt
 
     ! write current state to screen
-    print *, n, u
+    print *, n, h
 
   end do time_loop
 
